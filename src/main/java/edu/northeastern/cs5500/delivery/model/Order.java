c226344dcb74e1ae13a6805e93a26724c8c14c72
Danielle Mallare <dmallare7@gmail.com>
2020-10-19 13:38:19 -0700

    Completes basic validity checking for an Order

--- src/main/java/edu/northeastern/cs5500/delivery/model/Order.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/Order.java
@@ -3,7 +3,7 @@ package edu.northeastern.cs5500.delivery.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.bson.types.ObjectId;
-import java.util.HashMap;
+import java.util.*;
 import java.time.LocalDateTime;
 
 // @ Data annotation creates a constructor, getters, and setters
@@ -15,7 +15,7 @@ public class Order implements Model {
     private String currency;
     // TODO: In the future, replace String with Item for a
     // food item
-    private HashMap<String, Integer> items;
+    private ArrayList<HashMap<String, Integer>> items;
     private LocalDateTime orderTime;
     private LocalDateTime completionTime;
     // TODO: Make sure enum is completed & uncomment this
@@ -28,7 +28,6 @@ public class Order implements Model {
     private Integer restaurantID;
 
 
-
     /** 
     Validates the order. A valid order has a user, a valid associated restaurant,
     and at least one item.
@@ -39,7 +38,7 @@ public class Order implements Model {
         return user != null && !user.isEmpty()
                && restaurantID != null
                && !restaurantID.isEmpty()
-               && this.emptyOrderCheck()
+               && this.verifyOrderNonempty()
     }
 
     /**
@@ -47,7 +46,19 @@ public class Order implements Model {
     @return true if the order contains at least one item.
      */
     @JsonIgnore
-    private boolean emptyOrderCheck() {
-
+    private boolean verifyOrderNonempty() {
+        if (this.items.isEmpty()) {
+            return false
+        }
+        // If one item in the order, ensure quantity is at least 1
+        // TODO: we may just want to add this error checking to the Item itself!
+        if (this.items.size() == 1) {
+            HashMap<String, Integer> item = this.items.get(0);
+            for (String i : item.keySet()) {
+                if (item.get(i) < 1) {
+                    return False
+                }
+            }
+        }
+       return true;
     }
-}
