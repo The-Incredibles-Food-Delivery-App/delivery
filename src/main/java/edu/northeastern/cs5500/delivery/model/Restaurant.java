705a77b68f32e83a4a89843975e732d4e06e5227
cshortmorin <short-morin.c@husky.neu.edu>
2020-10-20 11:56:56 -0700

    filling out restaurant.java again

--- src/main/java/edu/northeastern/cs5500/delivery/model/Restaurant.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/Restaurant.java
@@ -1,4 +1,41 @@
+package edu.northeastern.cs5500.delivery.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+// @ Data annotation creates a constructor, getters, and setters
+@Data
 public class Restaurant implements Model {
+    private static final Double MAX_DISTANCE = 5.0;
+    
     private String restaurantName;
-    private String 
+    private String website;
+    // confused about the objectId vs restaurantId;
+    private ObjectId id;
+    private Integer restaurantId;
+    private Integer phoneNumber;
+    private String address;
+    // Uncomment this out after adding cuisine enum
+    // CuisineType cuisineType;
+    private String hours;
+    private ArrayList<ArrayList<Order>> pendingOrders;
+    private ArrayList<HashMap<String, String>> menu;
+
+    /** A valid restuarant should have a name, an Id, a phone number,
+     * hours, and a menu
+     * @return true if this is a valid Restaurant
+     */
+    @JsonIgnore
+    public boolean isValid() throws InvalidOrderException {
+        return restaurantId != null
+                && restaurantName != null
+                && phoneNumber != null
+                && menu != null
+                && hours != null;
+    }
+
 }
