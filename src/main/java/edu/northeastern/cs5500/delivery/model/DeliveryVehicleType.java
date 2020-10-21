b66f97f9508bebb25d33ab2daf0171f659b8f6ec
cshortmorin <short-morin.c@husky.neu.edu>
2020-10-21 14:04:23 -0700

    finished all the enums

---
+++ src/main/java/edu/northeastern/cs5500/delivery/model/DeliveryVehicleType.java
@@ -0,0 +1,6 @@
+package edu.northeastern.cs5500.delivery.model;
+
+public enum DeliveryVehicleType {
+    BIKE,
+    CAR;
+}
--- src/main/java/edu/northeastern/cs5500/delivery/model/OrderStatus.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/OrderStatus.java
@@ -1,1 +1,10 @@
+package edu.northeastern.cs5500.delivery.model;
 
+public enum OrderStatus {
+    NOT_CONFIRMED,
+    CONFIRMED,
+    IN_PROGRESS,
+    OUT_FOR_DELIVERY,
+    CANCELLED,
+    COMPLETED;
+}
