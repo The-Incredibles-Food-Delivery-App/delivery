2101fd731eef21647024c4f5acdafcfcf905ad3d
Danielle Mallare <dmallare7@gmail.com>
2020-10-23 19:31:17 -0700

    Completes draft of OrderController and Order

--- src/main/java/edu/northeastern/cs5500/delivery/model/Order.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/Order.java
@@ -1,23 +1,19 @@
 package edu.northeastern.cs5500.delivery.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.time.LocalDateTime;
+import java.util.*;
 import lombok.Data;
 import org.bson.types.ObjectId;
-import java.util.*;
-import java.time.LocalDateTime;
 
-// @ Data annotation creates a constructor, getters, and setters
 @Data
 public class Order implements Model {
     private static final Integer MAXIMUM_HOURS_ORDER_IN_ADV = 2;
+    private static final Integer MAXIMUM_DISTANCE = 35;
 
     private ObjectId id;
-    // TODO: create Receipt Model
-    // private Receipt receipt;
     private Double cost;
     private String currency;
-    // TODO: In the future, replace String with Item for a
-    // food item
     private ArrayList<HashMap<String, Integer>> items;
     private LocalDateTime orderTime;
     private LocalDateTime completionTime;
@@ -28,29 +24,52 @@ public class Order implements Model {
     // TODO: Make sure User class is completed & uncomment this
     // Can we use dep injection for this?
     // private User user;
-    private Integer restaurantID;
+    private Integer restaurantId;
     // TODO: work on payment Model and uncomment this
     // private CreditCard payment;
 
-
-    /** 
-    Validates the order. A valid order has a user, a valid associated restaurant,
-    and at least one item.
-    @return true if this order is valid.
+    /**
+     * Validates the order. A valId order has a user, a valid associated restaurant, at least one
+     * item, does not exceeded the maximum distance, and has a valid cost.
+     *
+     * @return true if this order is valid.
      */
     @JsonIgnore
     public boolean isValid() throws InvalidOrderException {
         // && !user.isEmpty() TODO: after user is created
-        // user != null 
-        return restaurantID != null
-               && this.restaurantID != null
-               && this.verifyOrderNonempty()
-               && this.verifyOrderByTime();
+        // user != null
+        return restaurantId != null
+                // TODO: this.restaurantId != null
+                && this.verifyOrderNonempty()
+                && this.verifyOrderByTime()
+                && this.verifyDistance()
+                && this.cost != null;
     }
 
     /**
-    Verifies that the order contains at least one item.
-    @return true if the order contains at least one item.
+     * Validates the distance. A valid distance is less than or equal to the maximum allowed
+     * distance
+     *
+     * @return true if the distance is less than or equal to the max delivery distance.
+     */
+    @JsonIgnore
+    public boolean verifyDistance() throws InvalidOrderException {
+        if (this.distance > MAXIMUM_DISTANCE) {
+            String message =
+                    "Distance exceeds maximum allowed delivery distance"
+                            + "Delivery distance cannot exceed"
+                            + MAXIMUM_DISTANCE
+                            + " miles.";
+            throw new InvalidOrderException(message);
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Verifies that the order contains at least one item.
+     *
+     * @return true if the order contains at least one item.
      */
     @JsonIgnore
     private boolean verifyOrderNonempty() {
@@ -67,14 +86,15 @@ public class Order implements Model {
                 }
             }
         }
-       return true;
+        return true;
     }
 
     /**
-    Verifies that the order contains a valid order by time.
-    @throws InvalidOrderException - if order time is before the current time
-        or more than the maximum number of hours from the current time.
-    @return true if a valid order by time has been set
+     * Verifies that the order contains a valId order by time.
+     *
+     * @throws InvalIdOrderException - if order time is before the current time or more than the
+     *     maximum number of hours from the current time.
+     * @return true if a valId order by time has been set
      */
     @JsonIgnore
     private boolean verifyOrderByTime() throws InvalidOrderException {
@@ -82,16 +102,20 @@ public class Order implements Model {
         // QUESTION: Do we want to throw an exception if set in the past?
         if (this.orderBy == null || this.orderBy.isBefore(LocalDateTime.now())) {
             this.orderBy = LocalDateTime.now();
-        } 
+        }
         // if orderBy data/time is set too far in advance, throw an exception
         if (this.orderBy.isAfter(LocalDateTime.now())) {
             if (this.orderBy.getDayOfWeek() != LocalDateTime.now().getDayOfWeek()
-               || this.orderBy.getHour() > LocalDateTime.now().getHour() + MAXIMUM_HOURS_ORDER_IN_ADV) {
-                   throw new InvalidOrderException("Please choose an order time that is within " + MAXIMUM_HOURS_ORDER_IN_ADV + " hours of the current time.");
-               }
+                    || this.orderBy.getHour()
+                            > LocalDateTime.now().getHour() + MAXIMUM_HOURS_ORDER_IN_ADV) {
+                throw new InvalidOrderException(
+                        "Please choose an order time that is within "
+                                + MAXIMUM_HOURS_ORDER_IN_ADV
+                                + " hours of the current time.");
+            }
         }
 
-       return true;
+        return true;
     }
 
 }
