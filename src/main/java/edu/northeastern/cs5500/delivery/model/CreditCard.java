c45fd2d130ce4c5188534430363f9dd31a7c030f
Mustafa Ramadan <72041999+mustafaramadan4@users.noreply.github.com>
2020-10-27 10:19:09 -0700

    Created a creditcard model class

---
+++ src/main/java/edu/northeastern/cs5500/delivery/model/CreditCard.java
@@ -0,0 +1,27 @@
+package edu.northeastern.cs5500.delivery.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+@Data
+public class CreditCard implements Model{
+  private ObjectId id;
+  private Integer cardNumber;
+  private LocalDate expirationDate;
+  private String username;
+  // TODO: isDefault was on the UML but unsure what this meant
+  private Boolean isDefault;
+
+  /**
+   * Checks that the CreditCard is valid, a valid creditCard has a nonnull cardNumber, non null expiration date and non null username.
+   *
+   * @return true if this CreditCard is valid
+   */
+  @JsonIgnore
+  public boolean isValid() {
+      return cardNumber != null && expirationDate != null && username != null;
+  }
+}
