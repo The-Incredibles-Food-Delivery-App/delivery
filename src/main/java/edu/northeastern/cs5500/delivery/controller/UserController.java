ba271eb26dd3479927f6b049e8d5e77b18b6dcbf
Danielle Mallare <dmallare7@gmail.com>
2020-10-31 22:36:53 -0700

    more cleanup to the user controller

--- src/main/java/edu/northeastern/cs5500/delivery/controller/UserController.java
+++ src/main/java/edu/northeastern/cs5500/delivery/controller/UserController.java
@@ -1,12 +1,12 @@
 package edu.northeastern.cs5500.delivery.controller;
 
 import com.mongodb.lang.Nullable;
-import edu.northeastern.cs5500.delivery.model.Customer;
+
 import edu.northeastern.cs5500.delivery.model.DeliveryDriver;
+import edu.northeastern.cs5500.delivery.model.Customer;
 import edu.northeastern.cs5500.delivery.model.Order;
 import edu.northeastern.cs5500.delivery.model.User;
 import edu.northeastern.cs5500.delivery.repository.GenericRepository;
-import java.util.Collection;
 import java.util.HashSet;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
@@ -20,12 +20,6 @@ public class UserController {
 
     private final GenericRepository<User> users;
 
-    // use these to keep track of the models that are
-    // registered with UserController
-    // TODO: DO WE NEED THESE?
-    // private ArrayList<DeliveryDriver> drivers;
-    // private ArrayList<Customer> customers;
-
     @Inject
     UserController(GenericRepository<User> userRepository) {
         users = userRepository;
@@ -38,34 +32,20 @@ public class UserController {
 
         log.info("UserController > construct > adding default users");
 
-        final DeliveryDriver defaultDeliveryDriver1 = new DeliveryDriver();
+        final User defaultDeliveryDriver1 = new DeliveryDriver();
         defaultDeliveryDriver1.setFirstName("Rachel");
         defaultDeliveryDriver1.setLastName("Woods");
         defaultDeliveryDriver1.setPhoneNumber("2245678921");
         defaultDeliveryDriver1.setUserName("Ra_wood");
         defaultDeliveryDriver1.setEmail("ra_wood@hotmail.com");
-        // defaultDeliveryDriver1.setCurrentOrder(defaultorder1);
-        defaultDeliveryDriver1.setCurrentlyWorking(true);
 
-        final Customer defaultCustomer1 = new Customer();
-        HashSet<Order> defaultCustomerOrders = new HashSet<>();
-        // defaultCustomerOrders.add(defaultorder1);
-        // defaultCustomerOrders.add(defaultorder2);
+        final User defaultCustomer1 = new Customer();
         defaultCustomer1.setFirstName("Sam");
         defaultCustomer1.setLastName("Rockwell");
         defaultCustomer1.setPhoneNumber("8892134567");
         defaultCustomer1.setUserName("sam_rockwell666");
         defaultCustomer1.setEmail("theOnlySamRockwell@gmail.com");
         defaultCustomer1.setAddress("333 Hollywood Blvd");
-        defaultCustomer1.setOrders(defaultCustomerOrders);
-
-        // Adding the Default Driver and Default Customer to an
-        // ArrayList of each type --->
-        // TODO: DO WE NEED THIS?
-        // drivers = new ArrayList<DeliveryDriver>();
-        // customers = new ArrayList<Customer>();
-        // drivers.add(defaultDeliveryDriver1);
-        // customers.add(defaultCustomer1);
 
         try {
             addUser(defaultDeliveryDriver1);
