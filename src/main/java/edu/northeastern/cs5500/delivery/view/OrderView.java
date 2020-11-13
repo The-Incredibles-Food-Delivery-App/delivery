1b71d82010169c8cadc892b9ecd2bf38355605ac
Mustafa Ramadan <72041999+mustafaramadan4@users.noreply.github.com>
2020-11-13 14:31:44 -0800

    Added OrderView

--- src/main/java/edu/northeastern/cs5500/delivery/view/OrderView.java
+++ src/main/java/edu/northeastern/cs5500/delivery/view/OrderView.java
@@ -1,5 +1,101 @@
 package edu.northeastern.cs5500.delivery.view;
 
-public class OrderView {
-  
+import static spark.Spark.delete;
+import static spark.Spark.get;
+import static spark.Spark.halt;
+import static spark.Spark.post;
+import static spark.Spark.put;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.northeastern.cs5500.delivery.JsonTransformer;
+import edu.northeastern.cs5500.delivery.controller.OrderController;
+import edu.northeastern.cs5500.delivery.model.Order;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.Or;
+
+@Singleton
+@Slf4j
+public class OrderView implements View {
+
+    @Inject
+    OrderView() {}
+
+    @Inject JsonTransformer jsonTransformer;
+
+    @Inject OrderController orderController;
+
+    @Override
+    public void register() {
+        log.info("OrderView > register");
+
+        get(
+                "/order",
+                (request, response) -> {
+                    log.debug("/order");
+                    response.type("application/json");
+                    return orderController.getOrders();
+                },
+                jsonTransformer);
+
+        get(
+                "/order/:id",
+                (request, response) -> {
+                    final String paramId = request.params(":id");
+                    log.debug("/order/:id<{}>", paramId);
+                    final ObjectId id = new ObjectId(paramId);
+                    Order order = orderController.getOrder(id);
+                    if (order == null) {
+                        halt(404);
+                    }
+                    response.type("application/json");
+                    return order;
+                },
+                jsonTransformer);
+
+        post(
+                "/order",
+                (request, response) -> {
+                    ObjectMapper mapper = new ObjectMapper();
+                    Order order = mapper.readValue(request.body(), Order.class);
+                    if (!order.isValid()) {
+                        response.status(400);
+                        return "";
+                    }
+
+                    // Ignore the user-provided ID if there is one
+                    order.setId(null);
+                    order = orderController.addOrder(order);
+
+                    response.redirect(
+                            String.format("/order/{}", order.getId().toHexString()), 301);
+                    return order;
+                });
+
+        put(
+                "/order",
+                (request, response) -> {
+                    ObjectMapper mapper = new ObjectMapper();
+                    Order order = mapper.readValue(request.body(), Order.class);
+                    if (!order.isValid()) {
+                        response.status(400);
+                        return "";
+                    }
+
+                    orderController.updateOrder(order);
+                    return order;
+                });
+
+        delete(
+                "/order",
+                (request, response) -> {
+                    ObjectMapper mapper = new ObjectMapper();
+                    Order order = mapper.readValue(request.body(), Order.class);
+
+                    orderController.deleteOrder(order.getId());
+                    return order;
+                });
+    }
 }
